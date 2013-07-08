98666ccad7b77ba0e23eaf17b1f9825dda657006
root <root@localhost.localdomain>
2013-07-08 21:41:06 +0800

    + [www] screenshot.png

--- test/mysql.php
+++ test/mysql.php
@@ -1,8 +1,8 @@
 <?php 
-mysql_connect('127.0.0.1','root','zzzizzz1');
+mysql_connect('127.0.0.1','root','Seattle01!');
 mysql_select_db('test');
-mysql_query('set names utf8');
+mysql_query('show databases');
 
-mysql_connect('127.0.0.1','root','zzzizzz1', true);
+mysql_connect('127.0.0.1','root','Seattle01!', true);
 mysql_select_db('test');
 mysql_query('set names utf8');
--- test/pdo.php
+++ test/pdo.php
@@ -1,7 +1,7 @@
 <?php 
 
-$db[0] = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', 'zzzizzz1');
-$db[1] = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', 'zzzizzz1');
+$db[0] = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', 'Seattle01!');
+$db[1] = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', 'Seattle01!');
 
 $db[0]->exec('set names utf8');
 $db[0]->query('set names utf8');
