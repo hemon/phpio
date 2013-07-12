51e2b51a8982216fc4f36a75333abffdd4a73b7e
Zhang wei <hemono@gmail.com>
2013-07-12 22:00:55 +0800

    * [test] remove own password

--- test/mysql.php
+++ test/mysql.php
@@ -1,8 +1,8 @@
 <?php 
-mysql_connect('127.0.0.1','root','Seattle01!');
+mysql_connect('127.0.0.1','root','');
 mysql_select_db('test');
 mysql_query('show databases');
 
-mysql_connect('127.0.0.1','root','Seattle01!', true);
+mysql_connect('127.0.0.1','root','', true);
 mysql_select_db('test');
 mysql_query('set names utf8');
