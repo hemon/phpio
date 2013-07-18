6dfb5ea3744a259dcc07a97c7ab1210745017a89
Zhangwei <hemono@gmail.com>
2013-07-18 17:51:37 +0800

    * [MiCurl] Refactoring

--- library/PHPIO/Hook/MiCurl.php
+++ library/PHPIO/Hook/MiCurl.php
@@ -4,17 +4,35 @@ This module for inject xiaomi X5 protocol
 */
 class PHPIO_Hook_MiCurl extends PHPIO_Hook_Curl {
 	function CURLOPT_POSTFIELDS_rewrite($value) {
-		if ( is_string($value) ) {
-			parse_str($value, $params);
-			if ( isset($params['data']) ) {
-				$params['data'] = json_decode(base64_decode($params['data']),true);
-				if ( is_array($params['data']) && isset($params['data']['header']['method']) ) {
-					$params['data']['header']['method'] .= '?XDEBUG_PROFILE='.PHPIO::$run_id;
-					$params['data'] = base64_encode(json_encode($params['data']));
-					$value = http_build_query($params);
-				}
+		do {
+			if ( !is_string($value) ) {
+				break;
 			}
-		}
+
+			parse_str($value, $params);
+			if ( !isset($params['data']) || !is_string($params['data']) ) {
+				break;
+			}
+
+			$params['data'] = $this->x5_decode($params['data']);
+			if ( !is_array($params['data']) || 
+				 !isset($params['data']['header']['method']) ) {
+				break;
+			}
+			// because the [gw] will combind header.url+header.method as new request URL
+			$params['data']['header']['method'] .= '?XDEBUG_PROFILE='.PHPIO::$run_id;
+			$params['data'] = $this->x5_encode($params['data']);
+			$value = http_build_query($params);
+		} while (0);
+
 		return $value;
+	}
+
+	function x5_encode($value) {
+		return base64_encode(json_encode($value));
+	}
+
+	function x5_decode($value) {
+		return json_decode(base64_decode($value), true);
 	}
 }
